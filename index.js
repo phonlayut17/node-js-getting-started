a48d2063eb8eb316eeb49e7788f61da2f5420304
phonlayut88 <phonlayut.sop@axonstech.com>
2023-06-07 14:47:37 +0700

    Add get methods

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/username/:usr', (req, res) => {
+    var usr_val = req.params.usr //ตัวแปรที่เก็บค่า username
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("get value /username/" + usr_val)
+    res.end()
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
